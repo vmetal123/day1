31bbbfbed0558d91d9452c5320e1aab4a852d3b8
vmetal123 <bladi22@yahoo.com>
2024-12-17 19:27:34 -0600

    day 1 part 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+var lines = File.ReadAllLines("input.txt");
+
+int[] left = [];
+int[] right = [];
+foreach (var line in lines)
+{
+    var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    left = [.. left, int.Parse(parts[0])];
+    right = [.. right, int.Parse(parts[1])];
+}
+
+Array.Sort(left);
+Array.Sort(right);
+
+int result = 0;
+for (int i = 0; i < left.Length; i++)
+{
+    result += Math.Abs(left[i] - right[i]);
+}
+
+Console
